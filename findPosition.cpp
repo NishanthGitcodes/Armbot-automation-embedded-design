f8668965a8a6aaa543e61bc0974dc9e0d3292248
Nishanth S <122718545+NishanthGitcodes@users.noreply.github.com>
2024-06-23 23:31:43 -0400

    First commit

--- findPosition.cpp
+++ findPosition.cpp
@@ -1,12 +1,12 @@
 #include "stdio.h"
 #include "math.h"
 
-double findX(double lengthOfTheArm,double angleWithAPlane){
+double findX(double lengthOfTheArm,double angleWithAPlane) {
     double radian = (3.141592653589793238462643/180)*angleWithAPlane;
     return (lengthOfTheArm*cos(radian));
 }
 
-double findY(double lengthOfTheArm,double angleWithAPlane){
+double findY(double lengthOfTheArm,double angleWithAPlane) {
     double radian = (3.141592653589793238462643/180)*angleWithAPlane;
     return (lengthOfTheArm*sin(radian));
 }
@@ -22,8 +22,12 @@ typedef struct {
     double angleOfAxialTurn;
 }Arms ;
 
-void initArmStructs(noOfArms){
+void initArmStructs(noOfArms) {
 Arms* LivePositions[noOfArms];
 Arms* targetPositions[noOfArms];
 }
 
+void setVertivalSwingAxisPositions(Arms* Arm, double angleWithXZplane, double lengthOfTheArm) {
+    Arm->lengthOfTheArm = lengthOfTheArm;
+    Arm->angleWithXzplane = angleWithXZplane(angleWithXZplane);
+}
