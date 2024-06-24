387f41760015aa56cbf66d617ff33d230c16e1f2
Nishanth S <122718545+NishanthGitcodes@users.noreply.github.com>
2024-06-23 23:22:34 -0400

    First commit

--- findPosition.cpp
+++ findPosition.cpp
@@ -1,25 +1,29 @@
+#include "stdio.h"
+#include "math.h"
 
-
-
-
-double findX(double r,double angle){
-    double rad = (3.141592653589793238462643/180)*angle;
-    return (r*cos(rad));
+double findX(double lengthOfTheArm,double angleWithAPlane){
+    double radian = (3.141592653589793238462643/180)*angleWithAPlane;
+    return (lengthOfTheArm*cos(radian));
 }
 
-double findY(double r,double angle){
-    double rad = (3.141592653589793238462643/180)*angle;
-    return (r*sin(rad));
+double findY(double lengthOfTheArm,double angleWithAPlane){
+    double radian = (3.141592653589793238462643/180)*angleWithAPlane;
+    return (lengthOfTheArm*sin(radian));
 }
 
 typedef struct {
+    double lengthOfTheArm;
     double endPointX;
     double endPointY;
     double endPointZ;
-    double lengthOfTheArm;
     double angleWithXZplane;
     double angleWithXYplane;
     double angleWithXYplane;
     double angleOfAxialTurn;
-    
 }Arms ;
+
+void initArmStructs(noOfArms){
+Arms* LivePositions[noOfArms];
+Arms* targetPositions[noOfArms];
+}
+
