7ab978fd8f8f05160eaa3f8ea8c00d8b8212d06a
Nishanth S <122718545+NishanthGitcodes@users.noreply.github.com>
2024-06-23 22:29:15 -0400

    first commit

--- findPosition.cpp
+++ findPosition.cpp
@@ -1,1 +1,24 @@
 
+
+
+
+double findX(double r,double angle){
+    double rad = (3.141592653589793238462643/180)*angle;
+    return (r*cos(rad));
+}
+
+double findY(double r,double angle){
+    double rad = (3.141592653589793238462643/180)*angle;
+    return (r*sin(rad));
+}
+
+typedef struct {
+    double endPointX;
+    double endPointY;
+    double endPointZ;
+    double lengthOfTheArm;
+    double angleWithXZplane;
+    double angleWithXYplane;
+    double angleWithXYplane;
+    double angleOfAxialTurn;
+}Arms ;
